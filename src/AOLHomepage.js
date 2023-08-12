559ab2bcd6b58ac5acab873b0ce9cba4c9abbd21
SacoyaAdams <114244564+SacoyaAdams@users.noreply.github.com>
2023-08-12 11:34:32 -0400

    added styling and profile picture

--- src/AOLHomepage.js
+++ src/AOLHomepage.js
@@ -1,41 +1,51 @@
 import React from 'react';
-import Typical from 'react-typical'
+import Typical from 'react-typical';
+import './AOLHomepage.css';
 
 export default function Profile() {
-    return(
-        <div className= "profile-details">
-        
+  return (
+    <>
+      <div className="profile-details">
+        <div className='colz'>
+            
+        </div>
         <div className="profile-details-name">
-            <span className="primary-text">
-                {" "}
-                Hello, I'm <span className="highlighted-text">SACOYA</span>
+          <span className="primary-text">
+            Hello, I'm <span className="highlighted-text">SACOYA</span>
+          </span>
+        </div>
+        <div className="profile-details-role">
+          <span className="primary-text">
+            <h1>
+              <Typical
+                loop={Infinity}
+                steps={[
+                  "Ethusiastic Dev",
+                  1000,
+                  "Full Stack Developer",
+                  1000,
+                  "Business Analyst",
+                  1000,
+                  "MERN STACK DEV",
+                  1000,
+                ]}
+              />
+            </h1>
+            <span className="profile-role-tagline">
+              knack of building applications with front and back end operations
             </span>
+          </span>
         </div>
-        <div className='profile-details-role'>
-            <span className="primary-text">
-                {" "}
-                <h1>
-                    <Typical 
-                    loop={Infinity}
-                    steps={[
-                        "Ethusiastic Dev ",
-                        1000,
-                        "Full Stack Deeveloper",
-                        1000,
-                        "Bussiness Analyst",
-                        1000,
-                        "MERN STACK DEV",
-                        1000,
-                    ]}
-                    />
-
-                </h1>
-                <span className='profile-role-tagline'>
-                    knack of building applications with front and back end operations
-
-                </span>
-            </span>
+        <div className="profile-options">
+          <button className="btn primary-btn">Hire Me</button>
+          <a href="Sacoya adams.pdf" download>
+            <button className="btn highlighted-btn">Get Resume</button>
+          </a>
         </div>
-        </div>
-    )
+      </div>
+      <div className="profile-picture">
+        <div className="profile-picture-background"></div>
+      </div>
+    </>
+  );
 }
