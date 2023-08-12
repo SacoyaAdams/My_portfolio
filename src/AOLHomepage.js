507360c433bd6f68a561d7cccdaef40ceffbdedf
SacoyaAdams <114244564+SacoyaAdams@users.noreply.github.com>
2023-08-12 10:09:10 -0400

    new style

--- src/AOLHomepage.js
+++ src/AOLHomepage.js
@@ -1,31 +1,41 @@
 import React from 'react';
-import './AOLHomepage.css'; // Import your CSS file for styling
+import Typical from 'react-typical'
 
-const AOLHomepage = () => {
-  return (
-    <div className="aol-homepage">
-      <header className="aol-header">
-        <img src="/aol-logo.png" alt="AOL Logo" className="aol-logo" />
-      </header>
-      <nav className="aol-nav">
-        <ul>
-          <li>Mail</li>
-          <li>News</li>
-          <li>Entertainment</li>
-          <li>Search</li>
-          <li>Weather</li>
-        </ul>
-      </nav>
-      <main className="aol-content">
-        {/* Content goes here */}
-        <h1>Welcome to AOL</h1>
-        <p>Your all-in-one destination for news, entertainment, and more!</p>
-      </main>
-      <footer className="aol-footer">
-        <p>&copy; {new Date().getFullYear()} AOL Inc. All rights reserved.</p>
-      </footer>
-    </div>
-  );
-};
+export default function Profile() {
+    return(
+        <div className= "profile-details">
+        
+        <div className="profile-details-name">
+            <span className="primary-text">
+                {" "}
+                Hello, I'm <span className="highlighted-text">SACOYA</span>
+            </span>
+        </div>
+        <div className='profile-details-role'>
+            <span className="primary-text">
+                {" "}
+                <h1>
+                    <Typical 
+                    loop={Infinity}
+                    steps={[
+                        "Ethusiastic Dev ",
+                        1000,
+                        "Full Stack Deeveloper",
+                        1000,
+                        "Bussiness Analyst",
+                        1000,
+                        "MERN STACK DEV",
+                        1000,
+                    ]}
+                    />
 
-export default AOLHomepage;
+                </h1>
+                <span className='profile-role-tagline'>
+                    knack of building applications with front and back end operations
+
+                </span>
+            </span>
+        </div>
+        </div>
+    )
+}
